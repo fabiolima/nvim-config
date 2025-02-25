180ea3c9f8e5f64d80aa809f36db0094acb2e7f6
Fábio de Lima <fabiolima@github.com>
2025-02-24 23:41:22 -0300

    add lazygit and show hidden files at neotree

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,24 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("LspAttachConflicts", { clear = true }),
+  desc = "Prevent tsserver and volar conflict",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local active_clients = vim.lsp.get_clients()
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client ~= nil and client.name == "volar" then
+      for _, c in ipairs(active_clients) do
+        if c.name == "ts_ls" then
+          c.stop()
+        end
+      end
+    end
+  end,
+})
