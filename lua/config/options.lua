9039a03af0aeec2e2c4a1b9997227f418f2f3d87
Fábio de Lima <fabiolimasalem@gmail.com>
2025-05-24 16:00:19 -0300

    replace ruby_lsp with solargraph

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,5 @@
 -- Add any additional options here
 
 vim.opt.swapfile = false
+-- vim.g.lazyvim_ruby_lsp = "solargraph"
+-- vim.g.lazyvim_ruby_formatter = "rubocop"
--- lua/plugins/ruby-lsp.lua
+++ lua/plugins/ruby-lsp.lua
@@ -1,16 +1,81 @@
+-- local util = require("lspconfig.util")
+
+-- return {
+--   default_config = {
+--     cmd = { 'solargraph', 'stdio' },
+--     settings = {
+--       solargraph = {
+--         diagnostics = true,
+--       },
+--     },
+--     init_options = { formatting = true },
+--     filetypes = { 'ruby' },
+--     root_dir = util.root_pattern('Gemfile', '.git'),
+--   },
+--   docs = {
+--     description = [[
+-- https://solargraph.org/
+--
+-- solargraph, a language server for Ruby
+--
+-- You can install solargraph via gem install.
+--
+-- ```sh
+-- gem install --user-install solargraph
+-- ```
+--     ]],
+--   },
+-- }
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     lazy = false,
+--     opts = {
+--       servers = {
+--         solargraph = {
+--           mason = false,
+--           cmd = { vim.fn.expand("~/.rbenv/shims/solargraph"), "stdio" },
+--         },
+--         rubocop = {
+--           mason = false,
+--           cmd = { vim.fn.expand("~/.rbenv/shims/rubocop"), "--lsp" },
+--         },
+--       },
+--     },
+--   },
+-- }
+local lspconfig = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
     opts = {
+      -- Useful for debugging formatter issues
+      format_notify = true,
       servers = {
-        ruby_lsp = {
+        -- rubocop = {
+        --   mason = false,
+        --   -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
+        --   -- cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        --   -- cmd = { vim.fn.expand("~/.rbenv/shims/rubocop"), "--lsp" },
+        --   -- root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        -- },
+        solargraph = {
           mason = false,
-          cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
-        },
-        rubocop = {
-          mason = false,
-          cmd = { vim.fn.expand("~/.rbenv/shims/rubocop"), "--lsp" },
+          -- See: https://medium.com/@cristianvg/neovim-lsp-your-rbenv-gemset-and-solargraph-8896cb3df453
+          cmd = { os.getenv("HOME") .. "/.rbenv/shims/solargraph", "stdio" },
+          -- root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+          settings = {
+            solargraph = {
+              autoformat = true,
+              completion = true,
+              diagnostics = true,
+              folding = true,
+              references = true,
+              rename = true,
+              symbols = true,
+            },
+          },
         },
       },
     },
