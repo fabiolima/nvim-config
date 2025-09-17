92e12a24447054112a353684083741b8a41deb6b
Fábio de Lima <fabiolimasalem@gmail.com>
2025-09-17 15:25:12 -0300

    add line numbers to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,17 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  config = {
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.cmd([[
+          setlocal relativenumber
+        ]])
+        end,
+      },
+    },
+  },
   opts = {
     filesystem = {
       filtered_items = {
